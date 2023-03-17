b9dc644a8f2a0f305af5dbdaf316590ba9dbb39b
JuliaTomchuk <yuliamalykowa@gmail.com>
2023-03-17 08:01:50 +0300

    initial commit

---
+++ src/test/java/org/tms/travel_agency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tms.travel_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
